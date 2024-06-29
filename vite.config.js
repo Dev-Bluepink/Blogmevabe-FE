befcff37ede90aa2f3b04faba280bb21348ed564
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-06-29 09:50:28 +0700

    config to render

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Đảm bảo rằng thư mục này tồn tại sau khi build
+    outDir: "dist",
   },
 });

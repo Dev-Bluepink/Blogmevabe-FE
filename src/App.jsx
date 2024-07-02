78277159b214479baa511746299dfa34431b6d5d
Vu Anh Tuan <tuanvuanhse140819@gmail.com>
2024-07-02 17:12:10 +0700

    upcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Suspense, lazy } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
 import AppLayout from "./components/AppLayout.jsx";
 import { AuthProvider } from "./components/AuthContext.jsx";
 import Error from "./components/Error.jsx";
@@ -13,7 +17,7 @@ import Profile2 from "./pages/profile2/Profile.jsx";
 import Knowledge from "./pages/knowledge/Knowledge.jsx";
 import Area from "./pages/area/Area.jsx";
 import Community from "./pages/community/Community.jsx";
-import BottomMenu from "./components/BottomMenu.jsx";
+// import ListPost from "./pages/listPost/ListPost.jsx";
 
 const Feed = lazy(() => import("./pages/feed/Feed.jsx"));
 const Profile = lazy(() => import("./pages/profile/Profile.jsx"));
@@ -114,7 +118,7 @@ const router = createBrowserRouter([
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 120000,
+      refetchOnWindowFocus: false,
     },
   },
 });
@@ -124,10 +128,7 @@ export function App() {
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <Suspense fallback={<Loader />}>
-          <RouterProvider router={router} fallbackElement={<Loader />}>
-            {/* bottom chỉ hiển thị khi ở màn hình điện thoại */}
-            <BottomMenu className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white md:hidden"></BottomMenu>
-          </RouterProvider>
+          <RouterProvider router={router} fallbackElement={<Loader />} />
         </Suspense>
       </AuthProvider>
     </QueryClientProvider>
